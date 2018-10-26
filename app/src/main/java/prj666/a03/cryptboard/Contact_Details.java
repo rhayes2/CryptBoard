67d0c2329bf5156a0da19cf928cb197af1ed1c7a
Muchtar Salimov <muchtarsalimov@gmail.com>
2018-10-26 18:53:25 -0400

    Added Button onclick events to contact details page (no function bodies)

--- app/src/main/java/prj666/a03/cryptboard/Contact_Details.java
+++ app/src/main/java/prj666/a03/cryptboard/Contact_Details.java
@@ -2,6 +2,8 @@ package prj666.a03.cryptboard;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -19,6 +21,9 @@ public class Contact_Details extends AppCompatActivity {
         TextView name = (TextView)findViewById(R.id.Contact_Deatil_name);
         TextView date = (TextView)findViewById(R.id.Contact_key_date);
         ImageView img = (ImageView)findViewById(R.id.Contact_Detail_Picture);
+        Button showQRButton = (Button) findViewById(R.id.showQRButton);
+        Button editContactButton = (Button) findViewById(R.id.editContactButton);
+        Button deleteContactButton = (Button) findViewById(R.id.deleteContactButton);
 
         if(tmp.getContactPubKey()!=null){
         try {
@@ -28,6 +33,34 @@ public class Contact_Details extends AppCompatActivity {
         }}
         name.setText(tmp.getName());
         date.setText(tmp.getDateCreated());
+
+
+        showQRButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // show QR code
+                // TO DO:
+                //       1) launch activity to show QR
+            }
+        });
+
+        editContactButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // edit contact activity
+                // TO DO:
+                //       1) launch edit contact activity
+                //       2) make sure this activity refreshes when coming back
+            }
+        });
+
+        deleteContactButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // delete contact code
+                // TO DO:
+                //       1) "are you sure" type popup
+                //       2) delete contact from database
+                //       3) return to previous activity
+            }
+        });
 
     }
 }
